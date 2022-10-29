6dd4a278d7ff25493960c0621d2438316e30ff95
ezpogue <ezpogue@gmail.com>
2022-10-29 05:11:33 -0700

    Changed puzzle to a full class with dimension, heuristic value and depth. Added queuing functions for both heuristic types

--- 8puzzle.py
+++ 8puzzle.py
@@ -4,14 +4,26 @@ import copy as c
 visited = set()
 inserted = set()
 maxqueue = 0
+depth = 0
 
-def hashkey(state):
-    key = ""
-    for i in range(len(state)):
-        for j in range(len(state[i])):
-            temp = chr(state[i][j] + 65)
-            key += temp
-    return key
+class slidepuzzle:
+
+    def __init__(self, dim, state, depth):
+        self.dim = dim
+        self.state = state
+        self.depth = depth
+        self.heuristic = 0
+
+    def hashkey(self):
+        key = ""
+        for i in range(self.dim):
+            for j in range(self.dim):
+                temp = chr(self.state[i][j] + 65)
+                key += temp
+        return key
+
+
+
 
 
 def generategoal(dim):
@@ -39,9 +51,16 @@ def generatefalse(dim):
     return false
 
 def findblank(puzzle):
+    for i in range(puzzle.dim):
+        for j in range(puzzle.dim):
+            if puzzle.state[i][j] == 0:
+                return (i, j)
+
+
+def findval(puzzle, val):
     for i in range(len(puzzle)):
-        for j in range(len(puzzle[i])):
-            if puzzle[i][j] == 0:
+        for j in range(len(puzzle)):
+            if puzzle[i][j] == val:
                 return (i, j)
 
 
@@ -56,20 +75,23 @@ def inputpuzzle(dim):
         puzzle.append(temp)
     return puzzle
 
+
 def generalsearch(problem, function, operators):
     nodes = q.Queue()
     nodes.put(problem)
-    goal = generategoal(len(problem))
+    goal = generategoal(problem.dim)
     while(1):
         if nodes.empty():
-            return generatefalse(len(problem))
+            return False
         curr = nodes.get()
-        visited.add(hashkey(curr))
-        if curr == goal:
-            return curr
+        visited.add(curr.hashkey())
+        if curr.state == goal:
+            global depth
+            depth = curr.depth
+            return True
         branches = function(curr, operators)
         for branch in branches:
-            nodes.put(c.deepcopy(branch))
+            nodes.put(branch)
         global maxqueue
         if nodes.qsize() > maxqueue:
             maxqueue = nodes.qsize()
@@ -80,54 +102,96 @@ def uniformcost(puzzle, operators):
     for op in operators:
         temp = c.deepcopy(puzzle)
         temp = op(temp)
-        if hashkey(temp) not in inserted:
-            branches.append(c.deepcopy(temp))
-            inserted.add(hashkey(temp))
+        if temp.hashkey not in inserted:
+            branches.append(slidepuzzle(temp.dim, temp.state, temp.depth+1))
+            inserted.add(temp.hashkey)
     return branches
 
 
-def left(problem):
-    i, j = findblank(problem)[0], findblank(problem)[1]
-    if j != 0:
-        problem[i][j], problem[i][j-1] = problem[i][j-1], problem[i][j]
-    return problem
+def misplacedtile(puzzle, operators):
+    branches = list()
+    for op in operators:
+        temp = c.deepcopy(puzzle)
+        temp = op(temp)
+        if temp.hashkey not in inserted:
+            branches.append(slidepuzzle(temp.dim, temp.state, temp.depth+1))
+            inserted.add(temp.hashkey)
+    branches = sorted(branches, key = lambda x: (mtheuristic(x)+x.depth))
+    return branches
 
 
-def right(problem):
-    i, j = findblank(problem)[0], findblank(problem)[1]
-    if j != len(problem) - 1:
-        problem[i][j], problem[i][j+1] = problem[i][j+1], problem[i][j]
-    return problem
+def manhattandistance(puzzle, operators):
+    branches = list()
+    for op in operators:
+        temp = c.deepcopy(puzzle)
+        temp = op(temp)
+        if temp.hashkey not in inserted:
+            branches.append(slidepuzzle(temp.dim, temp.state, temp.depth+1))
+            inserted.add(temp.hashkey)
+    branches = sorted(branches, key = lambda x: (mdheuristic(x)+x.depth))
+    return branches
+
+
+def mtheuristic(puzzle):
+    distance = 0
+    goal = generategoal(puzzle.dim)
+    for i in range(0, puzzle.dim):
+        for j in range(0, puzzle.dim):
+            if goal[i][j] != puzzle.state[i][j]:
+                distance += 1
+    return distance
+
+
+def mdheuristic(puzzle):
+    distance = 0
+    goal = generategoal(puzzle.dim)
+    for i in range(0, puzzle.dim):
+        for j in range(0, puzzle.dim):
+            if goal[i][j] != puzzle.state[i][j]:
+                k,l = findval(goal, puzzle.state[i][j])[0], findval(goal, puzzle.state[i][j])[1]
+                distance += abs(i - k) + abs(j - l)
+    return distance
 
 
 def up(problem):
     i, j = findblank(problem)[0], findblank(problem)[1]
     if i != 0:
-        problem[i][j], problem[i-1][j] = problem[i-1][j], problem[i][j]
+        problem.state[i][j], problem.state[i-1][j] = problem.state[i-1][j], problem.state[i][j]
     return problem
 
 
 def down(problem):
     i, j = findblank(problem)[0], findblank(problem)[1]
-    if i != len(problem) - 1:
-        problem[i][j], problem[i+1][j] = problem[i+1][j], problem[i][j]
+    if i != problem.dim - 1:
+        problem.state[i][j], problem.state[i+1][j] = problem.state[i+1][j], problem.state[i][j]
+    return problem
+
+
+def left(problem):
+    i, j = findblank(problem)[0], findblank(problem)[1]
+    if j != 0:
+        problem.state[i][j], problem.state[i][j-1] = problem.state[i][j-1], problem.state[i][j]
+    return problem
+
+
+def right(problem):
+    i, j = findblank(problem)[0], findblank(problem)[1]
+    if j != problem.dim - 1:
+        problem.state[i][j], problem.state[i][j+1] = problem.state[i][j+1], problem.state[i][j]
     return problem
 
 
 def main():
     operators = [up, down, left, right]
-    puzzle = inputpuzzle(3)
-    visited.add(hashkey(puzzle))
-    inserted.add(hashkey(puzzle))
-    puzzle = generalsearch(puzzle, uniformcost, operators)
-    for i in range(len(puzzle)):
-        for j in range(len(puzzle[i])):
-            print(puzzle[i][j], end = " ")
-        print()
-    print(hashkey(puzzle))
+    puzzle = slidepuzzle(3, inputpuzzle(3), 1)
+    visited.add(puzzle.hashkey())
+    inserted.add(puzzle.hashkey())
+    outcome = generalsearch(puzzle, manhattandistance, operators)
+    print(outcome)
     expanded = len(visited)
     print(expanded)
     print(maxqueue)
+    print(depth)
 
 
 main()
