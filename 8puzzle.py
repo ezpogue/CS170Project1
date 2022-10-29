ce5a910b67b5ae6fd0b6ed8c96e8ed1dc32bb586
ezpogue <ezpogue@gmail.com>
2022-10-29 04:06:01 -0700

    Added operators, the general search function as well as the uniform cost queueing function

--- 8puzzle.py
+++ 8puzzle.py
@@ -1,3 +1,10 @@
+import queue as q
+import copy as c
+
+visited = set()
+inserted = set()
+maxqueue = 0
+
 def hashkey(state):
     key = ""
     for i in range(len(state)):
@@ -22,6 +29,15 @@ def generategoal(dim):
         return goal
 
 
+def generatefalse(dim):
+    false = list()
+    for i in range(0, dim):
+        temp = list()
+        for j in range(0, dim):
+            temp.append(-1)
+        false.append(temp)
+    return false
+
 def findblank(puzzle):
     for i in range(len(puzzle)):
         for j in range(len(puzzle[i])):
@@ -40,13 +56,78 @@ def inputpuzzle(dim):
         puzzle.append(temp)
     return puzzle
 
+def generalsearch(problem, function, operators):
+    nodes = q.Queue()
+    nodes.put(problem)
+    goal = generategoal(len(problem))
+    while(1):
+        if nodes.empty():
+            return generatefalse(len(problem))
+        curr = nodes.get()
+        visited.add(hashkey(curr))
+        if curr == goal:
+            return curr
+        branches = function(curr, operators)
+        for branch in branches:
+            nodes.put(c.deepcopy(branch))
+        global maxqueue
+        if nodes.qsize() > maxqueue:
+            maxqueue = nodes.qsize()
+
+
+def uniformcost(puzzle, operators):
+    branches = list()
+    for op in operators:
+        temp = c.deepcopy(puzzle)
+        temp = op(temp)
+        if hashkey(temp) not in inserted:
+            branches.append(c.deepcopy(temp))
+            inserted.add(hashkey(temp))
+    return branches
+
+
+def left(problem):
+    i, j = findblank(problem)[0], findblank(problem)[1]
+    if j != 0:
+        problem[i][j], problem[i][j-1] = problem[i][j-1], problem[i][j]
+    return problem
+
+
+def right(problem):
+    i, j = findblank(problem)[0], findblank(problem)[1]
+    if j != len(problem) - 1:
+        problem[i][j], problem[i][j+1] = problem[i][j+1], problem[i][j]
+    return problem
+
+
+def up(problem):
+    i, j = findblank(problem)[0], findblank(problem)[1]
+    if i != 0:
+        problem[i][j], problem[i-1][j] = problem[i-1][j], problem[i][j]
+    return problem
+
+
+def down(problem):
+    i, j = findblank(problem)[0], findblank(problem)[1]
+    if i != len(problem) - 1:
+        problem[i][j], problem[i+1][j] = problem[i+1][j], problem[i][j]
+    return problem
+
 
 def main():
-    goal = inputpuzzle(3)
-    for i in range(len(goal)):
-        for j in range(len(goal[i])):
-            print(goal[i][j])
-    print(hashkey(goal))
+    operators = [up, down, left, right]
+    puzzle = inputpuzzle(3)
+    visited.add(hashkey(puzzle))
+    inserted.add(hashkey(puzzle))
+    puzzle = generalsearch(puzzle, uniformcost, operators)
+    for i in range(len(puzzle)):
+        for j in range(len(puzzle[i])):
+            print(puzzle[i][j], end = " ")
+        print()
+    print(hashkey(puzzle))
+    expanded = len(visited)
+    print(expanded)
+    print(maxqueue)
 
 
 main()
